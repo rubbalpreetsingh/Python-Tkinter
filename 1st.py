8032daeda40c606c7802039959a9cab4a96be98e
rubbalpreetsingh <rubbalpreetsingh03@gmail.com>
2021-05-29 23:32:15 +0530

    1st

---
+++ 1st.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root=Tk()
+frame=Frame(root,width=300,height=300)
+label=Label(root,text="HelloWorld")
+label.pack()
+frame.pack()      
+root.mainloop()   #mainloop == mainloop chaldi hai uddo tak ...jdo tak aapa uss window nu close nhi karda...eha sadi window nu bar bar show karda hai 
